5ae9cbedb7e26ba687da2d620b29918407ad0429
Daniel Marques <daniel23marq@gmail.com>
2024-04-28 16:18:12 +0100

    Criação do SkillsController e adição da API

---
+++ SkillPull/SkillPull/Controllers/API/SkillController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SkillPull.Data;
+using SkillPull.Models;
+
+namespace SkillPull.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillController : ControllerBase
+    {
+        public ApplicationDbContext _context;
+
+        public SkillController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            var listaDb = _context.Skills.ToList();
+            var listaRes = new List<SkillsDTO>();
+
+            foreach (var skill in listaDb)
+            {
+                SkillsDTO skilldto = new SkillsDTO();
+                skilldto.Descricao = skill.Descricao;
+                skilldto.Nome = skill.Nome;
+                skilldto.Tempo = skill.Tempo;
+                skilldto.Dificuldade = skill.Dificuldade;
+
+
+                listaRes.Add(skilldto);
+            }
+
+            return Ok(listaRes);
+        }
+
+
+        [HttpPost]
+        [Route("insereSkill")]
+        public ActionResult InsereSkill([FromBody] SkillsDTO dto)
+        {
+            Skills skill = new Skills();
+            skill.Nome = dto.Nome;
+            skill.Descricao = dto.Descricao;
+            skill.Tempo = dto.Tempo;
+            skill.Dificuldade = dto.Dificuldade;
+
+            _context.Skills.Add(skill);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("editSkill")]
+        public ActionResult EditSkill([FromBody] SkillsDTO dto, [FromQuery] int id)
+        {
+            Skills skill = _context.Skills.Where(c => c.SkillsId == id).FirstOrDefault();
+
+            skill.Nome = dto.Nome;
+            skill.Descricao = dto.Descricao;
+            skill.Tempo = dto.Tempo;
+            skill.Dificuldade = dto.Dificuldade;
+
+            _context.Skills.Update(skill);
+            _context.SaveChanges();
+
+
+            return Ok();
+        }
+
+
+        [Route("ola")]
+        public ActionResult Ola(string nome, string turma)
+        {
+            if (turma != "A" && turma != "B" && turma != "C")
+                return BadRequest();
+
+
+            return Ok("Olá" + nome + "da turma" + turma);
+        }
+    }
+
+ 
+}
---
+++ SkillPull/SkillPull/Models/SkillsDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace SkillPull.Models
+{
+    public class SkillsDTO
+    {
+        public string Nome { get; set; }
+
+        public string Dificuldade { get; set; }
+
+        public int Tempo { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
